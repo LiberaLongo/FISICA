ce93f4bf2372388bedc4a6c8211e32770cb9bccd
LiberaLongo <liberalongo@gmail.com>
2023-04-21 08:02:45 +0200

    le macchina di lab non sopportano le lettere accentate per i nomi dei file

--- sito-web/chapters.js
+++ sito-web/chapters.js
@@ -2,19 +2,18 @@ $( document ).ready(loadDOM());
 
 function loadDOM()
 {
-	var array = ["le misure", "moto in una dimensione", "forza e leggi di Newton", "moto in due e tre dimensioni", "applicazioni delle leggi di Newton", "quantità di moto", "sistemi di particelle", "cinematica dei moti rotatori", "dinamica dei moti rotatori", "momento angolare", "energia 1: lavoro ed energia cinetica", "energia 2: energia potenziale", "energia 3: conservazione dell'energia", "la gravitazione", "statica dei fluidi", "dinamica dei fluidi", "fenomeni oscillatori", "fenomeni ondulatori", "onde acustiche", "teoria della relatività ristretta", "temperatura", "proprietà molecolari dei gas", "prima legge della termodinamica", "entropia e seconda legge della termodinamica"];
+	var array = ["le misure", "moto in una dimensione", "forza e leggi di Newton", "moto in due e tre dimensioni", "applicazioni delle leggi di Newton", "quantita di moto", "sistemi di particelle", "cinematica dei moti rotatori", "dinamica dei moti rotatori", "momento angolare", "energia 1: lavoro ed energia cinetica", "energia 2: energia potenziale", "energia 3: conservazione dell'energia", "la gravitazione", "statica dei fluidi", "dinamica dei fluidi", "fenomeni oscillatori", "fenomeni ondulatori", "onde acustiche", "teoria della relativita ristretta", "temperatura", "proprieta molecolari dei gas", "prima legge della termodinamica", "entropia e seconda legge della termodinamica"];
 	let string = "<ul>";
 	let iterator = 0;
-	let index ="";
 
 	for (elem of array) 
 	{
 		iterator = iterator+1;
-		index = ( iterator < 10 ? "0" : "" ) + iterator ;
-		link = chapters/fisica-${index}_${elem}.html;
-                string += <li><button class="chap_button" type="button" onclick="func('${link}')" > cap ${index} </button> <b>${elem}</b></li>\n;
+		let index = ( iterator < 10 ? "0" : "" ) + iterator ;
+		link = `chapters/fisica-${index}_${elem}.html`;
+                string += `<li><button class="chap_button" type="button" onclick="func('${link}')" > cap ${index} </button> <b>${elem}</b></li>\n`;
         }
-        string += <li><a href='chapters/fisica-appendici.html'>appendici</a></li>;
+        string += `<li><a href='chapters/fisica-appendici.html'>appendici</a></li>`;
 	string += "</ul>";
 	$("#list").html(string);
 }
--- sito-web/notes/script.sh
+++ sito-web/notes/script.sh
@@ -3,7 +3,7 @@
 #WARNING! use it only the first time u want to create all files needed by the site, reuse clear all the content u created!
 
 cd chapters
-myArray=("le misure" "moto in una dimensione" "forza e leggi di Newton" "moto in due e tre dimensioni" "applicazioni delle leggi di Newton" "quantità di moto" "sistemi di particelle" "cinematica dei moti rotatori" "dinamica dei moti rotatori" "momento angolare" "energia 1: lavoro ed energia cinetica" "energia 2: energia potenziale" "energia 3: conservazione dell'energia" "la gravitazione" "statica dei fluidi" "dinamica dei fluidi" "fenomeni oscillatori" "fenomeni ondulatori" "onde acustiche" "teoria della relatività ristretta" "temperatura" "proprietà molecolari dei gas" "prima legge della termodinamica" "entropia e seconda legge della termodinamica")
+myArray=("le misure" "moto in una dimensione" "forza e leggi di Newton" "moto in due e tre dimensioni" "applicazioni delle leggi di Newton" "quantita di moto" "sistemi di particelle" "cinematica dei moti rotatori" "dinamica dei moti rotatori" "momento angolare" "energia 1: lavoro ed energia cinetica" "energia 2: energia potenziale" "energia 3: conservazione dell'energia" "la gravitazione" "statica dei fluidi" "dinamica dei fluidi" "fenomeni oscillatori" "fenomeni ondulatori" "onde acustiche" "teoria della relativita ristretta" "temperatura" "proprieta molecolari dei gas" "prima legge della termodinamica" "entropia e seconda legge della termodinamica")
 for i in ${!myArray[@]}; do
 	name=${myArray[$i]}
 	let "index=i+1"
